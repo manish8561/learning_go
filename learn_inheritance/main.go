9a78056267ee84ff0b29c5a25dc5a3c60f596aae
Manish Sharma <manish198646@yahoo.com>
2024-09-17 21:50:13 +0530

    add function closure

---
+++ learn_inheritance/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Person struct{
+	Name string
+	Age int
+}
+
+func (p *Person) Intro(){
+	fmt.Println(p.Name, p.Age)
+}
+
+type Employee struct {
+	*Person //Embedding
+	EmployeeId int
+}
+
+func (e *Employee) Intro(){
+	fmt.Println(e.Name, e.Age, e.EmployeeId)
+}
+
+func main(){
+	fmt.Println("Inheritance")
+
+	p := Person{Name: "Manish Sharma", Age:38}
+
+	e := Employee{Person: &p, EmployeeId: 1}
+
+	e.Intro()
+	// p.Intro()
+
+}
