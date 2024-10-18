ca1a1f03aae8a88c0424a452155585f6c18a0d10
Manish Sharma <manish198646@yahoo.com>
2024-10-18 06:38:46 +0530

    add a interview question

--- learn_basic/main.go
+++ learn_basic/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 /**
  * Interview questions:
-
   - What is escape analysis in golang?
     Escape analysis in Go is a compiler technique used to determine whether variables can be safely allocated on the stack or if they need to be moved to the heap.
 
@@ -34,6 +33,23 @@ import "fmt"
 	Grafarna
 	Logging
 	Prometheus
+*/
+
+/*
+*
+- How garbage collector works in golang?
+
+Below  how it works:
+
+Mark-and-Sweep Algorithm: Go uses a concurrent mark-and-sweep algorithm. During the "mark" phase, the GC identifies live objects (objects still in use) by traversing from root references (global variables, stacks). In the "sweep" phase, it frees the memory occupied by objects that were not marked.
+
+Generational Hypothesis: Go assumes most objects die young, so it focuses on collecting short-lived objects more frequently than long-lived ones.
+
+Concurrent Collection: The Go garbage collector runs concurrently with the application, which minimizes stop-the-world pauses. It pauses the program briefly at the start and end of the garbage collection cycle but runs alongside the program during the mark phase.
+
+Tri-color Marking: The GC uses a tri-color marking system (white, gray, black) to track the state of objects. White objects are unmarked (potentially garbage), gray objects are marked but not fully processed, and black objects are fully processed and known to be in use.
+
+Tuning and Optimization: You can tweak the GC's behavior using environment variables like GOGC (which controls the frequency of collections) or runtime functions to improve performance based on the application's memory usage patterns.
 */
 
 // defining constants
