30d107f1330ca289cfa1c5d8a2198eedd7a4adf4
Manish Sharma <manish198646@yahoo.com>
2025-12-31 16:44:03 +0530

    make change for pointer to pointer example

--- learn_pointers/main.go
+++ learn_pointers/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var a *int
 	var b *int
 
@@ -18,12 +18,25 @@ func main(){
 	d := new(int)
 	fmt.Println("Pointer with new keyword: ", &d)
 
+	//pointer to pointer
+	var e *int
+	var f **int
+	e = ptr(30)
+	f = &e
+
+	fmt.Println("value of e: ", *e)
+	fmt.Println("value of f: ", **f)
+	fmt.Println("memory address of e: ", &e)
+	fmt.Println("memory address of f: ", &f)
+	fmt.Println("memory address stored in f: ", f)
+	fmt.Println("value stored at the address stored in f: ", *f)
 }
-//generic after 1.18
-func ptrG[T any](v T) *T{
+
+// generic after 1.18
+func ptrG[T any](v T) *T {
 	return &v
 }
 
-func ptr(v int) *int{
+func ptr(v int) *int {
 	return &v
 }
