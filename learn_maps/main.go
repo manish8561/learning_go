476618b036a6bf9632197aa584599cd5a976c539
Manish Sharma <manish198646@yahoo.com>
2026-02-12 22:00:46 +0530

    remove map example folder and example to basic map

--- learn_maps/main.go
+++ learn_maps/main.go
@@ -33,4 +33,22 @@ func main() {
 	// using %#v to print the map with its type
 	fmt.Printf("using make function to create map map3: %#v", map3)
 	fmt.Println()
+
+	//more examples of map keys and values
+	example()
+}
+
+func example() {
+	//map keys are values accessed not referenced, so they can be pointers, but they are not dereferenced when used as keys.
+	var i *int
+	i = new(int)
+	*i = 42
+	fmt.Println(*i)
+
+	m := make(map[*int]interface{})
+	fmt.Println(m[i])
+
+	// n:=make(map[[]int]int) // invalid map key type []int
+	// map[[int]string]string // invalid map key type [int]string
+	// map[int]map[string]string // valid map key type int
 }
--- test_maps/go.mod
+++
@@ -1,3 +0,0 @@
-module example.com/u/demomaps
-
-go 1.24.0
